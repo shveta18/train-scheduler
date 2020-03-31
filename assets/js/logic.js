37563c1f05996622f61100b9ccc0f81e43f1341d
Shveta Kumar <sk@shveta.ca>
2020-03-31 02:35:54 -0400

    Added psudocode for moments

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,8 +10,16 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// Creating variable to reference the database
-var database = firebaseConfig.database;
+// Creating variable to reference the database. 
+//THIS LINE IS GENERATING AN ERROR FOR ME AND I AM UNABLE TO SOLVE THIS ISSUE
+var database = firebase.database();
+
+var name = "";
+var destination = "";
+var first = "";
+var frequency = "";
+
+
 
 // When the admin submits a new record for a train the input entered must be pulled and added to an array
 
@@ -30,7 +38,55 @@ $("#submit-button").on("click", function (event) {
     console.log("Train name: " + trainFirstTime);
     console.log("Train name: " + trainFrequency);
 
-    // once values are stored, push to the database inside an array
+    // convert the first instance of the train departing using moments
+    var trainFirstTimeConversion = moment(trainFirstTime, "HH:mm");
+    // current time 
+    console.log("CURRENT TIME: " + moment().format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(trainFirstTimeConversion), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % trainFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    //load data from array to database
+    database.ref().push({
+        name: trainName,
+        destination: trainDestination,
+        first: trainFirstTimeConversion, // use the converted value to be stored
+        frequency: trainFrequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP,
+        nextarrival: nextTrain
+    });
+
+
+    // clear text boxes once input added
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("");
+
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+
+    // Log everything that's coming out of snapshot
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().first);
+    console.log(childSnapshot.val().frequency);
+    console.log(childSnapshot.val().dateAdded);
+
 
 });
 
